1f23cbe61759ea8639aaef0be7723d61a8bd0e5a
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-05-11 14:44:23 +0300

    adjusted searchbar component's width

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -3,9 +3,9 @@ import React from "react"
 function Searchbar(){
 return (
 <form className="flex flex-col md:flex-row gap-3">
-    <div className="flex">
+    <div className="w-full pl-5 flex">
         <input type="text" placeholder="Search products"
-			className="w-full px-3 h-10 rounded-l border-2 border-black focus:outline-none focus:border-black"
+			className="w-3/5 px-3 h-10 rounded-l border-2 border-black focus:outline-none focus:border-black"
 			/>
         <button type="submit" className="bg-black border-black fill-white text-white rounded-r px-2 md:px-3 py-0 md:py-1">Search</button>
     </div>
